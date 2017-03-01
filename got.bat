5e08505ebd2267993d10c512ad56ed90813e3895
Kenneth Alexander <kenneth.alexander@defense.mrcy.com>
2017-03-01 14:52:21 -0500

    Support Python 3.6 64-bit in got.bat

--- got.bat
+++ got.bat
@@ -1,11 +1,24 @@
 @echo off
 REG QUERY "HKCU\Software\Python\PythonCore\3.6-32\InstallPath" > nul 2> nul
-if %ERRORLEVEL% neq 0 goto fail
+if %ERRORLEVEL% neq 0 goto trysixtyfourbit
 
 rem http://stackoverflow.com/a/13674631/309308 -- Not sure how consistent this is
 for /F "usebackq tokens=3*" %%A IN (`REG QUERY "HKCU\Software\Python\PythonCore\3.6-32\InstallPath" /ve`) DO (
 	set PYTHON_DIR=%%A %%B
 )
+
+"%PYTHON_DIR%\python" "%~dp0\got" %*
+goto end
+
+:trysixtyfourbit
+REG QUERY "HKCU\Software\Python\PythonCore\3.6\InstallPath" > nul 2> nul
+if %ERRORLEVEL% neq 0 goto fail
+
+rem http://stackoverflow.com/a/13674631/309308 -- Not sure how consistent this is
+for /F "usebackq tokens=3*" %%A IN (`REG QUERY "HKCU\Software\Python\PythonCore\3.6\InstallPath" /ve`) DO (
+	set PYTHON_DIR=%%A %%B
+)
+
 "%PYTHON_DIR%\python" "%~dp0\got" %*
 goto end
 
